ce7c5ac8fc6744573e2970f0199970ce314519a6
Jacob <jacob@britecore.com>
2022-11-03 12:20:38 -0500

    Deep load on boot to keep first request times down

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,7 +10,17 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.test.client import RequestFactory
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.base')
 
 application = get_wsgi_application()
+
+# Calling the application once to force loading before a request comes in
+test_env = RequestFactory()._base_environ(
+    PATH_INFO="/health/",
+    CONTENT_TYPE="text/html; charset=utf-8",
+    REQUEST_METHOD="GET",
+    HTTP_HOST="localhost"
+)
+application(test_env, lambda x, y: None)
