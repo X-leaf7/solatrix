040afd2bf986d8e865e5adff98c164155f8cc180
Jacob <jacob@britecore.com>
2022-11-02 22:59:19 -0500

    Add Allowed Hosts for AWS Internal IPs

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,38 @@
 from os import getenv
 
+import requests
+
 from .base import *
+
+
+EC2_PRIVATE_IPS = []
+METADATA_URI = getenv('ECS_CONTAINER_METADATA_URI_V4')
+
+ADMINS = (
+    ('Jacob', 'jacobef10@gmail.com'),
+)
+MANAGERS = (
+    ('Jacob', 'jacobef10@gmail.com'),
+)
+
+try:
+    resp = requests.get(METADATA_URI + '/task')
+    data = resp.json()
+
+    for container in data['Containers']:
+        for network in container['Networks']:
+            EC2_PRIVATE_IPS.extend(network['IPv4Addresses'])
+
+except Exception as e:
+    # silently fail as we may not be in an ECS environment
+    print(e)
+    pass
+
+if EC2_PRIVATE_IPS:
+    # Be sure your ALLOWED_HOSTS is a list NOT a tuple
+    # or .append() will fail
+    ALLOWED_HOSTS.extend(EC2_PRIVATE_IPS)
+
 
 DATABASES = {
     'default': {
