2d20bd085b5aa9f6a1de540e2b680d1647a224d9
Jacob <jacob@britecore.com>
2022-11-04 23:45:13 -0500

    Email must be unique if using for login

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 from config.models import SSBaseModel
 
 class User(AbstractUser, SSBaseModel):
+    email = models.EmailField(unique=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
