969dc9e910c98a224e7868597b9c0bca536accd1
Jacob <jacob@britecore.com>
2022-11-08 13:45:21 -0600

    Soft-delete users so that we can re-activate or deactivate users

--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,15 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User
 
-admin.site.register(User, BaseUserAdmin)
+@admin.register(User)
+class AllUsersAdmin(BaseUserAdmin):
+
+    list_display = ("username", "email", "is_active", "is_deleted")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups", "is_deleted")
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("Delete or Restore Account", {'fields': ('is_deleted',)}),
+    )
+
+    def get_queryset(self, request):
+        return User.global_objects.all()
--- users/models.py
+++ users/models.py
@@ -1,10 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django_softdelete.models import SoftDeleteModel, SoftDeleteManager
 
 from config.models import SSBaseModel
 
-class User(AbstractUser, SSBaseModel):
+
+class HybridUserManager(BaseUserManager, SoftDeleteManager):
+    pass
+
+
+class User(SoftDeleteModel, AbstractUser, SSBaseModel):
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = HybridUserManager()
