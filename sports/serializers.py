82963e8df9a57a4940f7006e98cf133f865f9eba
Jacob <jacob@britecore.com>
2022-11-13 17:28:41 -0600

    Add id to Serializers

--- sports/serializers.py
+++ sports/serializers.py
@@ -6,24 +6,24 @@ from .models import League, Sport, Stadium, Team
 class LeagueSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = League
-        fields = ['name', 'sport']
+        fields = ['id', 'name', 'sport']
 
 
 class SportSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Sport
-        fields = ['name', 'slug', 'leagues']
+        fields = ['id', 'name', 'slug', 'leagues']
 
 
 class StadiumSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Stadium
         lookup_field = 'slug'
-        fields = ['name', 'latitude', 'longitude']
+        fields = ['id', 'name', 'latitude', 'longitude']
 
 
 class TeamSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Team
         lookup_field = 'slug'
-        fields = ['name', 'sport', 'league', 'stadium']
+        fields = ['id', 'name', 'sport', 'league', 'stadium']
