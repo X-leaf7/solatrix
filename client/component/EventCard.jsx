ded631318bf86cd4cb8602557f2caed66cf455b6
Jacob <jacob@britecore.com>
2022-11-18 14:15:14 -0600

    Show lobby and event start times

--- client/component/EventCard.jsx
+++ client/component/EventCard.jsx
@@ -2,11 +2,15 @@ import React, { useState, useEffect, useContext } from 'react'
 import { Button } from 'react-bootstrap'
 import { DateTime } from 'luxon'
 import AppContext from '../context/AppContext'
-import Link from 'next/link'
 
 
 function EventCard({ event, setSelected, showJoin, showCreate }) {
     const { checkLogin } = useContext(AppContext);
+
+    const dateFormat = {
+        ...DateTime.DATETIME_FULL,
+        month: 'short'
+    }
 
     const showJoinChat = async () => {
         checkLogin(() => {
@@ -30,7 +34,8 @@ function EventCard({ event, setSelected, showJoin, showCreate }) {
                         <h3>{event.name}</h3>
                         <h4 className="text-primary">{event.home_team.name} vs {event.away_team.name}</h4>
 
-                        <p className="text-center my-2">{DateTime.fromISO(event.lobby_start_time).toLocaleString()}</p>
+                        <p className="text-center my-2">Lobby Opens @ {DateTime.fromISO(event.lobby_start_time).toLocaleString(dateFormat)}</p>
+                        <p className="text-center my-2">Event Starts @ {DateTime.fromISO(event.event_start_time).toLocaleString(dateFormat)}</p>
                         <p className="text-center mt-2 mb-4">Hosted by:  {event.host.username}</p>
                         {/* <hr /> */}
                         <div className="d-flex justify-content-evenly ">
