56b2d62d0e46f2f93d81db49d382efbb14ca287f
Jacob <jacob@britecore.com>
2022-11-24 22:59:31 -0600

    Clean up CreateChatModal

--- client/component/CreateChatModal.jsx
+++ client/component/CreateChatModal.jsx
@@ -26,38 +26,39 @@ function CreateChatModal({show, selectedEvent, onClose}) {
     return (
         <Modal centered id="1" className="S text-center" show={show} onHide={onClose}>
             <Modal.Header className="border-0 justify-content-center">
-                <Modal.Title><b>Create Private Chat?</b></Modal.Title>
+                <Modal.Title><b>{newEvent ? 'Success!' : 'Create Private Chat?'}</b></Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <form id="post-form" method="POST" action="#">
-                    <label><strong>A private room with a unique link will be created with you as the host.</strong></label>
-                    <p className="text-left mb-3">You and your guests can chat amongst yourselves during the game.</p>
-                    <p className="text-left mb-3">You can invite up-to 12 guests by sharing the link with them.</p>
-                    {
-                        newEvent && eventUrl ?
-                            <>
-                                <div>
-                                    <p>
-                                        <b>
-                                            <a href={eventUrl}>{eventUrl}</a>
-                                        </b>
-                                        <CopyToClipboard text={eventUrl} onCopy={() => setCopied(true)}>
-                                            <span className="fa-stack">
-                                                <i className='fa-regular fa-clone fa-stack-1x'></i>
-                                                { copied && <i className='fa-regular fa-check fa-stack-2x' style={{ color: 'green' }}></i>}
-                                            </span>
-                                        </CopyToClipboard>
-                                    </p>
-                                    <p>Share this link with your friends! You can find this link in your Account page as well.</p>
-
-                                </div>
-                            </>
-                        :
-                            <>
-                                <Button className="create-chat text-dark" onClick={() => handleClickCreateChat()}>Create Chat</Button>
-                            </>
-                    }
-                </form>
+                {
+                    newEvent && eventUrl ?
+                        <div>
+                            <div className="swal-icon swal-icon--success mt-0">
+                                <span className="swal-icon--success__line swal-icon--success__line--long"></span>
+                                <span className="swal-icon--success__line swal-icon--success__line--tip"></span>
+                                <div className="swal-icon--success__ring"></div>
+                                <div className="swal-icon--success__hide-corners"></div>
+                            </div>
+                            <p className="pt-2">
+                                <b>
+                                    <a href={eventUrl}>{eventUrl}</a>
+                                </b>
+                                <CopyToClipboard text={eventUrl} onCopy={() => setCopied(true)}>
+                                    <span className="fa-stack">
+                                        <i className='fa-regular fa-clone fa-stack-1x'></i>
+                                        { copied && <i className='fa-regular fa-check fa-stack-2x' style={{ color: 'green' }}></i>}
+                                    </span>
+                                </CopyToClipboard>
+                            </p>
+                            <p>Share this link with your friends! You can find this link in your Account page as well.</p>
+                        </div>
+                    :
+                        <div>
+                            <strong>A private room with a unique link will be created with you as the host.</strong>
+                            <p className="text-left mb-3">You and your guests can chat amongst yourselves during the game.</p>
+                            <p className="text-left mb-3">You can invite up-to 12 guests by sharing the link with them.</p>
+                            <Button className="create-chat text-dark" onClick={() => handleClickCreateChat()}>Create Chat</Button>
+                        </div>
+                }
             </Modal.Body>
         </Modal>
     )
