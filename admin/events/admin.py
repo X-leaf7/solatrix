1ea9faf1074f97c88cd36f89e861f9f0fd4b3ef5
Jacob <jacob@britecore.com>
2023-01-30 10:33:47 -0600

    Bring most recent events to the top of admin list

--- admin/events/admin.py
+++ admin/events/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from events.models import Event, Attendee
 
-admin.site.register(Event)
+
+class EventAdmin(admin.ModelAdmin):
+    date_hierarchy = 'event_start_time'
+    ordering = ['-event_start_time', '-lobby_start_time', 'name']
+
+
+admin.site.register(Event, EventAdmin)
 admin.site.register(Attendee)
