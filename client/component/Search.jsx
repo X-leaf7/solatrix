4ea0744449a1c06d7e874c9870989e93aedeea37
Jacob <jacob@britecore.com>
2023-02-13 14:59:56 -0600

    Clean up mobile nav padding

--- client/component/Search.jsx
+++ client/component/Search.jsx
@@ -73,7 +73,7 @@ function Search() {
         }
     }
     return (
-        <form onSubmit={handleSubmit} className="form-inline my-2 my-lg-0 d-flex align-items-center flex-nowrap">
+        <form onSubmit={handleSubmit} className="form-inline d-flex align-items-center flex-nowrap">
             <label className="sr-only">Search Team or Host</label>
             <div className='search-field'>
                 <input className="form-control me-sm-2" type="text"  value={search} onChange={handleChange} placeholder="Search" />
