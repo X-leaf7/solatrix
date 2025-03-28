e88bd74afbdbe6bd35d89836464938aaff5101c5
X-leaf7 <helloleaf.vil@gmail.com>
2025-03-29 06:27:14 +0900

    feat: update create/invite to chat room

--- client-mvp/app/(application)/chat/[id]/ChatPage.tsx
+++ client-mvp/app/(application)/chat/[id]/ChatPage.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import { ChatEvent, ChatHost } from '@/components';
 import ChatVideo from '@/components/01-cards/chat-video';
@@ -9,15 +9,6 @@ import { cx } from 'cva';
 import styles from './page.module.sass';
 
 const ChatPage = () => {
-  const [isClient, setIsClient] = useState(false)
-
-  useEffect(() => {
-    setIsClient(true)
-  }, [])
-
-  if (!isClient) {
-    return null
-  }
 
   return (
     <article className={cx(styles.base, styles['has-video'])}>
