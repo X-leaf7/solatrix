1bfebcc9d03e95d16429a9618fd3be2dc8d004cb
X-leaf7 <helloleaf.vil@gmail.com>
2025-03-28 10:26:15 +0900

    feat: config settings

--- admin/config/asgi.py
+++ admin/config/asgi.py
@@ -8,9 +8,27 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
 from django.core.asgi import get_asgi_application
+from django.conf import settings
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+import chat.routing
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.base')
 
-application = get_asgi_application()
+django_application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+    URLRouter(
+      chat.routing.websocket_urlpatterns
+    )
+  )
+})
+
