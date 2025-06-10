68a587e29552c19239ac8b32bf6565404d95417c
bestselection18 <noahsflood908@gmail.com>
2025-06-10 21:31:07 +0000

    feat: implement mail service config

--- admin/config/settings/base.py
+++ admin/config/settings/base.py
@@ -318,4 +318,10 @@ FIXTURE_DIRS = [
 
 APPEND_SLASH = False
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = env('EMAIL_BACKEND', default=None)
+EMAIL_HOST = env('EMAIL_HOST', default=None)
+EMAIL_PORT = env('EMAIL_PORT', default=None)
+EMAIL_USE_SSL = env('EMAIL_USE_SSL', default=None)
+EMAIL_HOST_USER = env('EMAIL_HOST_USER', default=None)
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default=None)
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default=None)
